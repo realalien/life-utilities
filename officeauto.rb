7a5bb5e439caa2e6609391a3824293c9bedd8bf8
realalien <realalien@99f39fa2-aecf-11dd-8748-9355f1163679>
2009-05-19 03:20:42 +0000

    added officeauto.rb

---
+++ officeauto.rb
@@ -0,0 +1,15 @@
+
+
+# Purpose: to automize as much as work possible, customization with intelli. 
+#          incl. info. collection, daily job automation, 
+#          working progress auditing, notes keeping (link all the info)
+#
+# Idea:
+# * some working style can be reused in other places, sharing the working
+#   style and workflow so that life is really incremental without losing
+#   experience and knowledge
+#
+
+
+
+
