3eaa16b5519b49f0265cbe0f8868a491ff720611
realalien <realalien@99f39fa2-aecf-11dd-8748-9355f1163679>
2009-05-25 11:46:32 +0000

    layout rebuilding

--- trunk/master.cfg.stepUI.rb
+++ trunk/master.cfg.stepUI.rb
@@ -16,6 +16,7 @@ class StepStatus
 		self.line_num  = no
 		self.name = name 
 		self.status = status
+		@cbx_map_step = []
 	end
 end
 
@@ -24,27 +25,68 @@ class BuildbotFrame < Frame
 		super(nil,
 			  :title => "Steps For Build Master",
 			  :pos => [150, 150],
-			  :size => [ 300, 200 ]
+			  :size => [ 400, 300 ]
 			 )
-		
+        @main_sizer = BoxSizer.new(VERTICAL)
+        self.set_sizer(@main_sizer)		
+	# --------------   upper panel  -----------------------
 	@panel = Panel.new(self)
+	@main_sizer.add(@panel, 1 , Wx::GROW|Wx::TOP, 1  , nil )
 
 #	file_loc_lbl = StaticText(panel, -1, "master.cfg location:", 
 #							  DEFAULT_POSITION, DEFAULT_SIZE, ALIGN_CENTER) 
-    @file_loc_txt = TextCtrl.new(@panel, -1)
+        @file_loc_txt = TextCtrl.new(@panel, -1)
 	@file_chooser_btn = Button.new(@panel,
-								 id => -1,
-								 label => "Choose a master.cfg")
+								 -1,
+								 "Choose a master.cfg")
+    @upper_sizer = BoxSizer.new(HORIZONTAL)
+	@panel.set_sizer(@upper_sizer)
+	@upper_sizer.add(@file_loc_txt, 0, GROW|EXPAND, 1 , nil)
+    @upper_sizer.add(@file_chooser_btn, 0, GROW|EXPAND, 1 , nil)
+	# -------------   middle checkboxes panel --------------
+	# the panel holding all the checkboxes(addStep line of cfg)
+	@mid_panel = Panel.new(self)
+    @main_sizer.add(@mid_panel, 5 , Wx::GROW|Wx::EXPAND, 5 , nil )
+ 
+   @demo_text = TextCtrl.new(@mid_panel, -1, "anchored in middle panel" )
+						 # DEFAULT_POSITION, DEFAULT_SIZE, 3 )	 
 
-	@apply_change_btn = Button.new(@panel,
-								  id => -1 , 
-								  label => "Apply change!")
+    @mid_sizer = BoxSizer.new(VERTICAL)
+	@mid_panel.set_sizer(@mid_sizer)
 
-	evt_button(@file_chooser_btn.get_id()) { |event| read_cfg(event) }
-
-#	@panel_cbx = Panel.new(self) # holding all the checkboxes(addStep line of cfg)
+   # @mid_sizer.add(@demo_text, 1 , Wx::GROW|Wx::EXPAND, 1 , nil )
+	# ======    testing section  ============
+	@boxes = []
+	(1..5).each do | i |
+	#	step_cbx = CheckBox.new(@mid_panel, i, "Step No."+i.to_s)
+	#	@boxes << step_cbx   
 	end
 
+    #@boxes.each do | step |
+	#	@mid_sizer.add(step , 1, GROW , 1, nil)
+	#end
+
+	# ======================================
+
+	# -------------   bottom panel  -------------
+	@bottom_panel = Panel.new(self)
+	@main_sizer.add(@bottom_panel, 1 , Wx::GROW|Wx::BOTTOM, 5 , nil )
+    
+	@preview_change_btn = Button.new(@bottom_panel, -1, "Preview changes")
+	@apply_change_btn = Button.new(@bottom_panel,
+								   -1 , 
+								   "Apply change!")
+
+	@bottom_panel.set_sizer(@bottom_sizer)
+	@bottom_sizer = BoxSizer.new(HORIZONTAL)
+	@upper_sizer.add(@preview_change_btn, 0, GROW|EXPAND, 1 , nil)
+    @upper_sizer.add(@apply_change_btn, 0, GROW|EXPAND, 1 , nil)
+	evt_button(@file_chooser_btn.get_id()) { |event| read_cfg(event) }
+
+
+	end
+
+	# read the master.cfg file and produce controls related
 	def read_cfg(event)
 		fd = FileDialog.new(nil)
 		fd.show_modal
@@ -52,15 +94,44 @@ class BuildbotFrame < Frame
 		# filename = fd.get_filename
 		@file_loc_txt.write_text(path)
 		@steps = load_steps_from_master_cfg(path) # TODO: info (line_num, step name, on/off status)
+		# =====   inspect section  =====
 		str = ""
 		@steps.each do |step|
 			str += "#{step.name} , #{step.status}\n"
 		end
 		@file_loc_txt.write_text(str)
+		# ==============================
+		@steps.each do | step |
+			# TODO: shall I maintain the status in memory?
+			step_cbx = CheckBox.new(@mid_panel, -1 , "Step "+ step.name)
+		    evt_checbox(step_cbx.get_id) { | event | maintain_map(event) }
+			@cbx_map_step.store(step_cbx , step)
+			step_cbx.set_value(false) if step.status == false
+			step_cbx.set_value(true) if step.status == true
+			@boxes << step_cbx
+		end	
+
+		# paint on graphic
+		@boxes.each do | step_cbx |
+			@mid_sizer.add(step_cbx, 1, GROW , 1, nil)
+		end
+
+		# produce collections of checkboxes,  TODO,  is there MVC code, javabean protocal I can follow?
+		
 	end
 
 	def write_cfg(event)
 			
+	end
+
+	# SUG: http://wxruby.rubyforge.org/doc/eventhandlingoverview.html
+	def maintain_map(event)
+		if @cbx_map_step.size == 0
+			MessageDialog.new(nil, "no cbx => step mapping found!", 
+			return
+		end
+
+		if 
 	end
 
 	def load_steps_from_master_cfg(file)
@@ -94,6 +165,10 @@ class BuildbotFrame < Frame
 	end
 end
 
+# experiment, test if we can add non-graphic properties, maybe it's not good choice, some layers of code may be better.
+#class CustomizedCheckbox < Checkbox
+		
+#end
 
 class BuildbotStepsQuickSwitch < App
 	def on_init
